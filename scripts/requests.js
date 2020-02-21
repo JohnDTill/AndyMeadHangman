856212c8cc69e7afdc9ff5bc07993540c0fb3fd8
John Till <johndtill@gmail.com>
2020-02-21 14:46:48 -0800

    Changed to HTTPS fetch

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,5 +1,5 @@
 const getPuzzlePromise = async (wordCount = 1) => {
-    const response = await fetch(`http://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    const response = await fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     if(response.status !== 200) throw new Error(`An HTTP error has taken place`)
     const data = await response.json()
     return data.puzzle
